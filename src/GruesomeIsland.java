7609b12dcb6313039a733fcf0946e0441e467984
TheDunco <duncvan@gmail.com>
2020-02-21 19:52:24 -0500

    Fixed biomeselection

--- src/GruesomeIsland.java
+++ src/GruesomeIsland.java
@@ -4,10 +4,6 @@ import java.lang.Object;
 import java.lang.String;
 import java.util.*;
 
-public class GruesomeIsland {
-
-}
-
 public class World {
 
     // Create a 26x26 array of chunks
@@ -15,41 +11,47 @@ public class World {
     
     public class Chunk(Biome biome) {
         public static Biome biome = biome;
+        public int elevation;
 
     }
     
     public class Biome(int biomeSelection) {
-        public int soundMod = 5;
-        public int tracksMod;
-        public int coverMod;
+        public int defaultMod = 5;
+        public int soundMod = defaultMod;
+        public int tracksMod = defaultMod;
+        public int coverMod = defaultMod;
         public String biomeName;
 
         public int fixedMod = 2;
 
         // Biome switching
-        switch (biomeSelection) {
-        case 1: // winter
-            biomeName = "Winter";
-            soundMod -= fixedMod;
-            tracksMod +=  fixedMod;
-            cover -= fixedMod;
-        case 2: // desert
-            biomeName = "Desert";
-            tracksMod +=  fixedMod;
-            cover -= fixedMod;
-        case 3: // forest
-            biomeName = "Forest";
-            soundMod -= fixedMod;
-            cover += fixedMod;
-        case 4:
-            biomeName = "City";
-            soundMod += fixedMod;
-            tracksMod -=  fixedMod;
-            cover += fixedMod;
-        default:
-            biomeName = "Forest (default)";
-            soundMod -= fixedMod;
-            cover += fixedMod;
+        public void biomeSelect() {
+            switch(biomeSelection) {
+                case 1: // winter
+                    biomeName = "Winter";
+                    soundMod -= fixedMod;
+                    tracksMod +=  fixedMod;
+                    cover -= fixedMod;
+                case 2: // desert
+                    biomeName = "Desert";
+                    tracksMod +=  fixedMod;
+                    cover -= fixedMod;
+                case 3: // forest
+                    biomeName = "Forest";
+                    soundMod -= fixedMod;
+                    cover += fixedMod;
+                case 4:
+                    biomeName = "City";
+                    soundMod += fixedMod;
+                    tracksMod -=  fixedMod;
+                    cover += fixedMod;
+                default:
+                    biomeName = "Forest (default)";
+                    soundMod -= fixedMod;
+                    cover += fixedMod;
+            }
+
+        }
 
         public int getSoundMod() {
             return soundMod;
